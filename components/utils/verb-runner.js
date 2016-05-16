5e6a4406b2a66efae02a9faac2fd58ff6fff6a3e
Kyle Shockey <kyle.shockey1@gmail.com>
2016-05-16 13:35:51 -0700

    Promise-based sequence runner

--- components/utils/verb-runner.js
+++ components/utils/verb-runner.js
@@ -1,78 +1,106 @@
 var exec = require('child_process').spawnSync;
 var expandTilde = require('expand-tilde');
+var Promise = require('bluebird');
+
 
 function run(slackbot, bundle, verb, target) {
   slackbot.send({
     channel: bundle.msg.channel,
     text: `i'm on it! 👍`
-  }).then(() => {
-    var result = execSequence(verb.sequence, target);
-
-    var outputLog = '';
-    result.outs.forEach(output => outputLog += output + '\n');
-
-    if(result.success) {
-      // report a sequence success
-      slackbot.createPost({
-        content: outputLog,
-        comment: `<@${bundle.msg.user}> \`${bundle.cmd.join(' ')}\` succeeded!`,
-        filename: `${bundle.cmd.join(' ')} at ${Date()}`
-      });
-    } else {
-      slackbot.createPost({
-        content: outputLog,
-        comment: `<@${bundle.msg.user}> \`${bundle.cmd.join(' ')}\` failed.`,
-        filename: `${bundle.cmd.join(' ')} at ${Date()}`
-      });
-    }
   })
-}
-
-function execSequence(sequence, target) {
-
-  var hasSequenceFailed = false;
-
-  var outs = []; // stdouts and stderrs
-
-  sequence.forEach(cmd => {
-    if(hasSequenceFailed) {
-      return;
-    }
-
-    var bin = cmd[0];
-    var args = cmd.slice(1);
-    var res = exec(bin, args || [], {
-      cwd: expandTilde(target.workingDir),
-      shell: true
-    });
-
-    var commandSuccess = res.status === 0;
-
-
-    var out = '';
-
-    out += `>>> ${cmd.join(' ')} \n`;
-
-    if(commandSuccess) {
-      out += `${res.stdout.toString()}`;
-      if(res.stderr) {
-        out += (res.stderr.toString())
-      }
-    } else {
-      out += (`${res.stderr ? res.stderr.toString() : res.error.code}`);
-      hasSequenceFailed = true;
-    }
-
-    outs.push(out);
-  })
-
-  // at this point, the sequence has finished
-
-  return {
-    success: !hasSequenceFailed,
-    outs
-  }; // return true for success, false for failure
-
+    .then(execSequence.bind(null, verb.sequence, target))
+    .then(processResult)
+    .then(sendOutput.bind(null, {slackbot, bundle}));
 }
 
 module.exports = {run};
+
+function execSequence(sequence, target) {
+  var outs = []; // stdouts and stderrs
+  var promiseArray = [];
+
+  // prepare the function-wrapped promise array
+  sequence.forEach(cmd => {
+    promiseArray.push(function() {
+      return executeInShell({
+        binary: cmd[0],
+        args: cmd.slice(1),
+        target
+      });
+    });
+  })
+
+  // kick off the unwrap step
+  // console.log(unwrapPromises({arr: promiseArray}));
+  return unwrapPromises({arr: promiseArray});
+}
+
+function unwrapPromises({arr, i = 0, results = []}) {
+  var fn = arr[i];
+  if(arr[i+1]) {
+    return fn()
+      .then(res => {
+        results.push(res);
+        return unwrapPromises({arr, i: i + 1, results});
+      });
+  } else {
+    return fn()
+      .then(res => {
+        results.push(res);
+        return new Promise(r => r(results));
+      });
+  }
+}
+
+function executeInShell({binary, args, target}) {
+  return new Promise((resolve, reject) => {
+    var execRes = exec(binary, args || [], {
+      cwd: expandTilde(target.workingDir),
+      shell: true
+    });
+    resolve({
+      execRes,
+      cmd: (`${binary} ${args.join(' ')}`)
+    });
+  })
+}
+
+function processResult(outputs) {
+  console.log(outputs);
+
+  var isSuccessful = true;
+  var outputLog = '';
+
+  outputs.forEach(output => {
+    outputLog += `>>> ${output.cmd} \n`;
+
+    if(output.execRes.status === 0) {
+      outputLog += `${output.execRes.stdout.toString()}`;
+      if(output.execRes.stderr) {
+        outputLog += (output.execRes.stderr.toString())
+      }
+    } else {
+      outputLog += (`${output.execRes.stderr ? output.execRes.stderr.toString() : output.execRes.error.code}`);
+      isSuccessful = false;
+    }
+  })
+
+  return {outputLog, isSuccessful};
+}
+
+function sendOutput({slackbot, bundle}, {outputLog, isSuccessful}) {
+  if (isSuccessful) {
+    // report a sequence success
+    slackbot.createPost({
+      content: outputLog,
+      comment: `<@${bundle.msg.user}> \`${bundle.cmd.join(' ')}\` succeeded!`,
+      filename: `${bundle.cmd.join(' ')} at ${Date()}`
+    });
+  } else {
+    slackbot.createPost({
+      content: outputLog,
+      comment: `<@${bundle.msg.user}> \`${bundle.cmd.join(' ')}\` failed.`,
+      filename: `${bundle.cmd.join(' ')} at ${Date()}`
+    });
+  }
+}
